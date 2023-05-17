185448ec6c340b729a06456041444494b459a92d
tayk6ix <89669855+tayk6ix@users.noreply.github.com>
2023-05-16 20:06:52 -0500

    HeroImage Added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='bg-[url("/DesktopImg1.jpeg")] heroLanding heroHeight text-white text-center flex flex-col items-center justify-center gap-4 tablet:mx-[2.5vw] tablet:px-[5vw] tablet:text-left tablet:items-start'>
+    <div className='bg-[url("/StockPhotos/HeroImage.jpeg")] heroLanding heroHeight text-white text-center flex flex-col items-center justify-center gap-4 tablet:mx-[2.5vw] tablet:px-[5vw] tablet:text-left tablet:items-start'>
       <h1 className='font_title leading-tight w-4/5 font-semibold tablet:font-bold tablet:leading-none tablet:w-[58%]'>Together, <br className='hidden tablet:block'/> let's build your dream</h1>
       <Link className='heroBtn font_small orangeBG w-3/5 py-2 px-8 tablet:w-fit' href='#mini_gallery'>Discover More</Link>
     </div>
