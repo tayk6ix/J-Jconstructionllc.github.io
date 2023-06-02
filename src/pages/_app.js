5c50b874db44cb00fb2bc39d76445b019f87086c
tayk6ix <89669855+tayk6ix@users.noreply.github.com>
2023-06-02 03:05:38 -0500

    1.11
    Vercel Analytics addition

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,9 +5,16 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 
+import {Analytics} from '@vercel/analytics/react'
+
 config.autoAddCss = false; 
 
 export default function App({ Component, pageProps }) {
-  return  <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics/>
+    </>
+  ) 
 
 }
